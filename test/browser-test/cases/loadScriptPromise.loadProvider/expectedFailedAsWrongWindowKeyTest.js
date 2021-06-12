a24a818bd60603dd51e6917440e16e5a9e6e94b2
horseluke <horseluke@126.com>
2021-06-13 00:18:52 +0800

    修改：代码

--- test/browser-test/cases/loadScriptPromise.loadProvider/expectedFailedAsWrongWindowKeyTest.js
+++ test/browser-test/cases/loadScriptPromise.loadProvider/expectedFailedAsWrongWindowKeyTest.js
@@ -1,10 +1,10 @@
-describe('loadScriptPromise.loadProvider expected failed as Wrong windowKey test', async function() {
+describe('loadScriptPromise.loadProvider expected failed as Wrong windowKey in loadScriptPromise.setProvider test', async function() {
 
     const providerName = "not-exist-windowkey";
 
     loadScriptPromise.setProvider(providerName, {
         src: "./static/testlibNotExistWindowKey@0.0.1/testlibNotExistWindowKey.js",
-        windowKey: "not-exist-windowkey-1100000",
+        windowKey: "not-exist-windowkey-1100000",    //Set the wrong value that window object does not have
     });
     
     it('is load failed as expected because provider option windowKey is wrong value. Error message must begin with "\'windowKey\' is used for search specific provider in window object, but found nothing."', async function() {
