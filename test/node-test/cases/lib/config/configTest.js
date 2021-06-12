c81dace289ff6a56fda28d997e4e658d4f6c2e82
horseluke <horseluke@126.com>
2021-06-12 21:56:44 +0800

    增加：config test测试

---
+++ test/node-test/cases/lib/config/configTest.js
@@ -0,0 +1,37 @@
+import BootstarpService from "../../../lib/service/BootstarpService.js";
+
+
+describe('Module config Test', async function() {
+
+    const config = await BootstarpService.instance.ESMImportFromAppdirAndReturnDefault("lib/config/config.js");
+
+    it('is get with default value in Module config', async function(done) {
+        const defaultVal = Math.random();
+        const getValue = config.get("not_exists_key__________", defaultVal);
+        done(defaultVal == getValue ? null : new Error("get with default value failed"));
+    });
+
+
+    it('is set one value in Module config', async function(done) {
+        const configKey = "key_test_11111112222222";
+        const value = Math.random();
+        config.set(configKey, value);
+        const getValue = config.get(configKey);
+        done(value == getValue ? null : new Error("set one value failed"));
+    });
+
+    it('is set object values in Module config', async function(done) {
+
+        const configKey = "key_test_11111112222222_objd";
+        const value = Math.random();
+        const obj = {};
+        obj[configKey] = value;
+
+        config.set(obj);
+
+        const getValue = config.get(configKey);
+        done(value == getValue ? null : new Error("set object values failed"));
+    });
+
+});
+
