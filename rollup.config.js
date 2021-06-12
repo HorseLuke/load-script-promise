c0285e454e4652d130157919d1db666ee64552c0
horseluke <horseluke@126.com>
2021-06-12 21:24:15 +0800

    增加：load script

--- rollup.config.js
+++ rollup.config.js
@@ -3,26 +3,41 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
+
+const output = {
+    file: './dist/loadScriptPromise.js',
+    format: 'umd',
+    name: 'loadScriptPromise',
+    sourcemap: false
+};
+
+if(env == "production"){
+    output.file = "./dist/loadScriptPromise.min.js";
+    output.sourcemap = true;
+}
+
+const plugins = [
+    babel({
+        exclude: 'node_modules/**',
+        "runtimeHelpers": true,
+    })
+];
+
+if(env == "production"){
+    plugins.push(terser());
+}
+
+plugins.push(resolve());
+plugins.push(commonjs());
+
+
 
 
 const config = {
     input: './index.js',
-    output: {
-        file: './dist/loadScriptPromise.js',
-        format: 'umd',
-        name: 'loadScriptPromise',
-        sourcemap:true
-    },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**',
-            "runtimeHelpers": true,
-        }),
-        //terser(),
-        resolve(),
-        commonjs(),
-    ]
+    output: output,
+    plugins: plugins
 };
 
 
