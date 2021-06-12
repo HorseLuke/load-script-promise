b24f193776eadb8f9a49a365c7a5e7628bd8aa1a
horseluke <horseluke@126.com>
2021-06-13 03:35:45 +0800

    删除：config测试中，无用的async

--- test/browser-test/cases/loadScriptPromise.config/configTest.js
+++ test/browser-test/cases/loadScriptPromise.config/configTest.js
@@ -1,19 +1,19 @@
 describe('loadScriptPromise.config Test', async function() {
 
-    it('is get with default value in Module config', async function(done) {
+    it('is get with default value in Module config', function(done) {
         const defaultVal = Math.random();
         const getValue = loadScriptPromise.config.get("not_exists_key__________", defaultVal);
         done(defaultVal == getValue ? null : new Error("get with default value failed"));
     });
 
     
-    it('is get all config values in Module config', async function(done) {
+    it('is get all config values in Module config', function(done) {
         const d = loadScriptPromise.config.get();
         done(typeof d === "object" ? null : new Error("get all config values failed"));
     });
 
 
-    it('is set one value in Module config', async function(done) {
+    it('is set one value in Module config', function(done) {
         const configKey = "key_test_11111112222222";
         const value = Math.random();
         loadScriptPromise.config.set(configKey, value);
@@ -21,7 +21,7 @@ describe('loadScriptPromise.config Test', async function() {
         done(value == getValue ? null : new Error("set one value failed"));
     });
 
-    it('is set object values in Module config', async function(done) {
+    it('is set object values in Module config', function(done) {
 
         const configKey = "key_test_11111112222222_objd";
         const value = Math.random();
